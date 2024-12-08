1f461ad345d703fcc52dd2d42f247d1679a936c3
Anupam0-0 <anupamchoudhary04@gmail.com>
2024-12-08 15:04:05 +0530

    Added Rest of Static Pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetter from "../components/NewsLetter";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          src={assets.about_img}
+          alt=""
+          className="w-full md:max-w-[450px]"
+        />
+
+        <div className="flex flex-col -mt-4 justify-center gap-6 md:w-2/4 text-slate-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe fuga
+            consequatur dicta consequuntur asperiores, ipsa, iusto aliquam, quas
+            recusandae hic cumque impedit. Optio cupiditate porro temporibus qui
+            asperiores molestias, repellat repudiandae itaque perferendis
+            placeat nostrum voluptatum. Architecto doloremque itaque
+            exercitationem quidem facilis iure ad nemo vero explicabo eos
+            blanditiis error doloribus deleniti, beatae eum? Rerum.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            blanditiis minima tempore. Deleniti nemo, unde nihil accusantium
+            exercitationem error optio vero esse. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Accusantium, alias. Sunt.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio est
+            adipisci in, quo dolorem aperiam eligendi possimus, aliquid vero
+            laboriosam nisi accusantium veritatis officia vitae?
+          </p>
+        </div>
+      </div>
+
+      {/* ------------- WHY CHOOSE ------------- */}
+
+      <div className="text-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            We meticulously select and vet each product to ensure they meet the
+            highest standards of reliability and excellence.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            With our user-friendly interface and hassle-free services, we make
+            your shopping experience smooth and enjoyable.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Our team of dedicated professionals is here to help with any queries
+            or issues, ensuring your satisfaction every step of the way.
+          </p>
+        </div>
+      </div>
+
+      <NewsLetter />
+
+    </div>
+  );
+};
+
+export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+import Title from '../components/Title'
+import NewsLetter from '../components/NewsLetter'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} tetx2={'US'} /> 
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-20'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6 '>
+          <p className='font-semibold text-xl text-gray-600'>Out Store</p>
+          <p className='text-slate-500 '> 5XX% Street Near Moon Street <br /> Over Jupiter alley </p>
+          <p className='text-slate-500 '> Tel: 909090909 <br/> Email:stombland@gmail.com </p>
+          <p className='font-semibold text-xl text-gray-600'> Careers at Mono Store</p>
+          <p className='teext-gray-500 '>Learn more about our teams and job openings</p>
+          <button className='border-black border px-8 py-4 text-sm hover:bg-black hover:text-white translate-all duration-500' >Explore Jobs</button>
+        </div>
+      </div>
+
+      <NewsLetter />
+
+    </div>
   )
 }
 
